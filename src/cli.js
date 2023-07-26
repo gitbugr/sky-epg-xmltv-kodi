c23874ae54030252b980e1aae76b94c87e8ebf72
Kyron Taylor <kyaltada@pm.me>
2023-07-26 10:16:41 +0100

    ~ update packages, ~ determine output type from env vars

--- src/cli.js
+++ src/cli.js
@@ -1,9 +1,21 @@
-import SkyEPGScraper from './skyEPGScraper'
+import SkyEPGScraper from './skyEPGScraper';
+import os from 'os';
 
-const scraper = new SkyEPGScraper('file');
-scraper.run().then(() => {
-    scraper.write();
-}).catch((error) => {
-    process.stderr.write(`howthefuck: ${error}`);
-});
+let outputType;
 
+if (process.env.GIST_TOKEN && process.env.GIST_ID && process.env.GIST_FILENAME) {
+    outputType = 'gist';
+} else if (process.env.OUTPUT_DIRECTORY && process.env.OUTPUT_FILENAME) {
+    outputType = 'file';
+} else {
+    process.stderr.write('You\'re missing some environment variables. Please define either GIST_* or OUTPUT_*.' + os.EOL);
+}
+
+if (outputType) {
+    const scraper = new SkyEPGScraper(outputType);
+    scraper.run().then(() => {
+        scraper.write();
+    }).catch((error) => {
+        process.stderr.write(`howthefuck: ${error}`);
+    });
+}
